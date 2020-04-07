b0a816ff61738c3ae41e3188ef51825a7e7ec435
karl <215574300@qq.com>
2020-04-07 15:19:03 +0800

    netty整合

---
+++ netty-server/src/main/java/org/karl/NettyServer.java
@@ -0,0 +1,82 @@
+package org.karl;
+
+import com.alibaba.fastjson.JSON;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.karl.handler.CustomNettyHandler;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @Author: KARL ROSE
+ * @Date: 2020/4/7 11:27
+ **/
+@Slf4j
+public class NettyServer {
+
+    private static final int PORT = 8888;
+
+    public static void main(String[] args) throws InterruptedException {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        NioEventLoopGroup boos = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            serverBootstrap
+                    .group(boos, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(PORT))
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) {
+
+                            /**
+                             *
+                             * 1.HttpRequestDecoder，用于解码request
+                             * 2.HttpResponseEncoder，用于编码response
+                             * 3.aggregator，消息聚合器（重要）。为什么能有FullHttpRequest这个东西，就是因为有他，HttpObjectAggregator，如果没有他，就不会有那个消息是FullHttpRequest的那段Channel，同样也不会有FullHttpResponse。
+                             * 如果我们将 HttpObjectAggregator(512 * 1024)的参数含义是消息合并的数据大小，如此代表聚合的消息内容长度不超过512kb。
+                             * 4.添加我们自己的处理接口
+                             *
+                             **/
+                            ch.pipeline()
+                                    .addLast("decoder", new HttpRequestDecoder())   // 1
+                                    .addLast("encoder", new HttpResponseEncoder())  // 2
+                                    .addLast("aggregator", new HttpObjectAggregator(512 * 1024))    // 3
+                                    .addLast("handler", new CustomNettyHandler());
+
+                            /*ch.pipeline()
+                                    .addFirst(new LoggingHandler(LogLevel.DEBUG))
+                                    .addLast(new HttpRequestDecoder(), new StringDecoder())
+                                    .addLast(new ChannelInboundHandlerAdapter() {
+                                        @Override
+                                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                            log.info(JSON.toJSONString(ctx));
+                                            super.channelActive(ctx);
+                                        }
+                                    })
+                                    .addLast(new CustomNettyHandler());*/
+                        }
+                    }).option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, Boolean.TRUE);
+
+
+                    /*.childOption(ChannelOption.AUTO_READ, false);
+            ChannelFuture f = serverBootstrap.bind().sync();
+            f.channel().closeFuture().sync();*/
+        }  finally {
+            boos.shutdownGracefully().sync();
+        }
+
+
+    }
+}
---
+++ netty-server/src/main/java/org/karl/handler/CustomNettyClientHandler.java
@@ -0,0 +1,31 @@
+package org.karl.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @Author: KARL ROSE
+ * @Date: 2020/4/7 12:20
+ **/
+public class CustomNettyClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.write(Unpooled.copiedBuffer("Netty Workd", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+        System.out.println("Client Received: " + ByteBufUtil.hexDump(msg.readBytes(msg.readableBytes())));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
