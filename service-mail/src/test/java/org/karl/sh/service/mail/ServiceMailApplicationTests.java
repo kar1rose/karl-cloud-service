0906298758eb2e2c227c0fb9d8e66c7a1e141fd7
karl.rose <215574300@qq.com>
2020-04-21 14:08:32 +0800

    hello ~

--- service-mail/src/test/java/org/karl/sh/service/mail/ServiceMailApplicationTests.java
+++ service-mail/src/test/java/org/karl/sh/service/mail/ServiceMailApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.karl.sh.service.mail.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
