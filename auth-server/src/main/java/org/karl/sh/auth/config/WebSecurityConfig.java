7aed9b07390df527f057c036cdcacdc33ff264fa
karl <215574300@qq.com>
2020-09-15 15:58:33 +0800

    调整授权服务redisConnectionFactory的实现类型

--- auth-server/src/main/java/org/karl/sh/auth/config/WebSecurityConfig.java
+++ auth-server/src/main/java/org/karl/sh/auth/config/WebSecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * web安全配置
  *
@@ -33,9 +35,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-//                .csrf().disable().exceptionHandling()
-//                .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
+        http.csrf().disable().exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
                 .authorizeRequests().antMatchers("/oauth/**").permitAll()
                 .anyRequest().authenticated()
                 .and().httpBasic();
--- auth-server/src/main/java/org/karl/sh/auth/config/db/RedisConfiguration.java
+++ auth-server/src/main/java/org/karl/sh/auth/config/db/RedisConfiguration.java
@@ -1,23 +1,18 @@
 package org.karl.sh.auth.config.db;
 
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
-import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.io.IOException;
 import java.time.Duration;
 
 /**
@@ -31,7 +26,7 @@ public class RedisConfiguration {
 
     private static final Logger logger = LoggerFactory.getLogger(RedisConfiguration.class);
 
-    @Bean
+    /*@Bean
     public RedissonClient redisson() throws IOException {
         //从自定义配置文件加载redis集群配置
         Config config = Config.fromYAML(new ClassPathResource("config/redisson.yml").getInputStream());
@@ -42,7 +37,7 @@ public class RedisConfiguration {
     public RedissonConnectionFactory redisConnectionFactory(RedissonClient redisson) {
         RedissonConnectionFactory redissonConnectionFactory = new RedissonConnectionFactory(redisson);
         return redissonConnectionFactory;
-    }
+    }*/
 
     @Bean
     public StringRedisSerializer stringRedisSerializer() {
@@ -62,20 +57,20 @@ public class RedisConfiguration {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(factory);
         // key的序列化类型
-//        redisTemplate.setKeySerializer(stringRedisSerializer());
+        redisTemplate.setKeySerializer(stringRedisSerializer());
         redisTemplate.setValueSerializer(jackson2JsonRedisSerializer());
         // value的序列化类型
-//        redisTemplate.setHashKeySerializer(stringRedisSerializer());
+        redisTemplate.setHashKeySerializer(stringRedisSerializer());
         redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer());
         return redisTemplate;
     }
 
-    /*@Bean
+    @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
         StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
         stringRedisTemplate.setConnectionFactory(factory);
         return stringRedisTemplate;
-    }*/
+    }
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
