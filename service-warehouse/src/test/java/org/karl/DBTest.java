385037a4e664dde9446a6e25dc53499168312f60
karl <215574300@qq.com>
2020-09-27 18:36:58 +0800

    V20200927

---
+++ service-warehouse/src/test/java/org/karl/DBTest.java
@@ -0,0 +1,37 @@
+package org.karl;
+
+
+import java.sql.*;
+
+/**
+ * @author KARL ROSE
+ * @date 2020/8/12 15:22
+ **/
+public class DBTest {
+
+    public static void main(String[] args) throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://139.224.83.117/karl?characterEncoding=utf8&useUnicode=true&useSSL=false", "root", "P@ssw0rd@2020");
+        Statement statement = connection.createStatement();
+        ResultSet resultset = statement.executeQuery("select * from goods");
+        ResultSetMetaData metaData = resultset.getMetaData();
+        int count = metaData.getColumnCount();
+        for (int i = 1; i <= count; i++) {
+            String colName = metaData.getColumnName(i);
+            System.out.print(colName + "      ");
+        }
+        System.out.println();
+        System.out.println("------------------------------------");
+        while (resultset.next()) {
+            for (int i = 1; i <= count; i++) {
+                String value = resultset.getString(metaData.getColumnName(i));
+                System.out.print(value + "    ");
+            }
+            System.out.println();
+        }
+        resultset.close();
+        statement.close();
+        connection.close();
+
+    }
+}
--- service-warehouse/src/test/java/org/karl/Test.java
+++ service-warehouse/src/test/java/org/karl/Test.java
@@ -1,37 +1,58 @@
 package org.karl;
 
-
-import java.sql.*;
-
 /**
  * @author KARL ROSE
- * @date 2020/8/12 15:22
+ * @date 2020/9/23 15:38
  **/
 public class Test {
 
-    public static void main(String[] args) throws Exception {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://139.224.83.117/karl?characterEncoding=utf8&useUnicode=true&useSSL=false", "root", "P@ssw0rd@2020");
-        Statement statement = connection.createStatement();
-        ResultSet resultset = statement.executeQuery("select * from goods");
-        ResultSetMetaData metaData = resultset.getMetaData();
-        int count = metaData.getColumnCount();
-        for (int i = 1; i <= count; i++) {
-            String colName = metaData.getColumnName(i);
-            System.out.print(colName + "      ");
-        }
-        System.out.println();
-        System.out.println("------------------------------------");
-        while (resultset.next()) {
-            for (int i = 1; i <= count; i++) {
-                String value = resultset.getString(metaData.getColumnName(i));
-                System.out.print(value + "    ");
-            }
-            System.out.println();
-        }
-        resultset.close();
-        statement.close();
-        connection.close();
+    public int minOperationsMaxProfit(int[] customers, int boardingCost, int runningCost) {
+        /*HashMap<Integer, Integer> map = new HashMap<>();
+        int maxProfit = 0;
+        int profit;
+        int uploadedCustomer = 0;
+        int runNum = 0;
 
+        for (int customer : customers) {
+            int upCustomer = Math.min(customer, 4);
+            uploadedCustomer += upCustomer;
+            while (upCustomer <= customer) {
+                runNum++;
+                profit = uploadedCustomer * boardingCost - runNum * runningCost;
+                maxProfit = Math.max(maxProfit, profit);
+                map.put(maxProfit, runNum);
+                if (upCustomer == customer) {
+                    break;
+                }
+                if (customer - upCustomer < 4) {
+                    uploadedCustomer += (customer - upCustomer);
+                    upCustomer = customer;
+                } else {
+                    upCustomer += 4;
+                    uploadedCustomer += 4;
+                }
+            }
+        }
+        return maxProfit > 0 ? map.get(maxProfit) : -1;*/
+        int profit = 0, res = -1, rem = 0;
+        int round = 0, sum = 0, n = customers.length;
+        int i = 0;
+        while(i < n || rem > 0) {
+            rem += i < n ? customers[i++]: 0;
+            int cur = Math.min(rem, 4);
+            rem -= cur;
+            round += 1;
+            sum += cur;
+            int cost = sum * boardingCost - runningCost * round;
+            if(cost > profit) {
+                profit = cost;
+                res = round;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Test().minOperationsMaxProfit(new int[]{10, 10, 6, 4, 7}, 3, 8));
     }
 }
